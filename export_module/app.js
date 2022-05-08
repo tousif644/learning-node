b1cb2e3ef50b648e26f1186e21f7ada8d75cdfcf
tousif644 <tousif2567@gmail.com>
2022-05-08 09:17:37 +0600

    first commit

---
+++ export_module/app.js
@@ -0,0 +1,13 @@
+module.exports = {
+  x: 10,
+  y: 20,
+  z: function run() {
+    const obj = {
+      name: "Tousif Tasrik",
+      roll: "548",
+      address: "Sarkarpara,Gaibandha",
+      college: "Dr. Maleka College",
+    };
+    return obj;
+  },
+};
