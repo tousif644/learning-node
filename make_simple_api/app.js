4879983a785f0df8b14a49a3d0e6b662a1108dd8
tousif644 <tousif2567@gmail.com>
2022-05-09 05:50:40 +0600

    Fourth and Fifth commit

---
+++ make_simple_api/app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const data = require('./data');
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "content-type": "applicationjson" });
+  res.write(JSON.stringify(data));
+  res.end();
+}).listen(5000);
+    
---
+++ make_simple_api/data.js
@@ -0,0 +1,7 @@
+const data = [
+  { name: "John smith", occupation: "FrontEnd Developer", location: "UK" },
+  { name: "Steven Duncan", occupation: "Backend Developer", location: "USA" },
+  { name: "Clinton Clerk", occupation: "DevOps", location: "FRANCE" },
+];
+
+module.exports = data;
